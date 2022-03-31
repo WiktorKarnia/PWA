701e8f1eae0e3aaef2564921be200d614ba63583
WiktorKarnia <wiktor1341@o2.pl>
2022-03-31 23:03:24 +0200

    test

---
+++ js/func.js
@@ -0,0 +1,18 @@
+function vibrate_sos() {
+    navigator.vibrate([300,100,300,100,300,100,600,100,600,100,600,100,300,100,300,100,300]);
+  }
+  
+  var x = document.getElementById("demo");
+  
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+  }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-function vibrate_sos() {
-  navigator.vibrate([300,100,300,100,300,100,600,100,600,100,600,100,300,100,300,100,300]);
-}
+window.onload = () => {
+  'use strict';
 
-var x = document.getElementById("demo");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
   }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
 }
