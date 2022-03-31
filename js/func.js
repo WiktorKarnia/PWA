ce6b4f5e4b8635bd3eb5e9d6d8e05fc2d8a271c9
WiktorKarnia <wiktor1341@o2.pl>
2022-03-31 23:28:44 +0200

    idk

--- js/func.js
+++ js/func.js
@@ -3,7 +3,7 @@ function vibrate_sos() {
 }
   
 var x = document.getElementById("demo");
-  
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
