7d3db6ed01d64ebab3efc0ef5d54290729deb229
WiktorKarnia <wiktor1341@o2.pl>
2022-03-31 19:48:45 +0200

    vibrations test

--- js/main.js
+++ js/main.js
@@ -6,3 +6,37 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+function vibrate_sos() {
+  navigator.vibrate([300,100,300,100,300,100,600,100,600,100,600,100,300,100,300,100,300]);
+}
+
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
